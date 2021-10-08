b60eedefbf0c7ea9792201587c91437a1547308c
Leo <leocabibihan@outlook.com>
2021-10-08 13:00:49 +0300

    can take one deadline as input

---
+++ README.md
@@ -0,0 +1,34 @@
+## Features
+
+### implement static deadlines only
+
+create the form that request for title, date and time fields
+
+the form can be hidden on click
+
+the data for each element
+
+```js
+const data = [
+  {
+    type: "deadline",
+    deadline: new Date(year, month, day, hour, minute, second),
+    title: "finish x hw",
+  },
+  {
+    type: "deadline",
+    deadline: new Date(year, month, day, hour, minute, second),
+    title: "finish x hw",
+  },
+];
+```
+
+### save deadlines to local storage
+
+each element can also be removed
+
+### implement repeating deadlines
+
+somehow resets the deadline everyday
+
+### drag and drop?
--- index.js
+++ index.js
@@ -1,7 +1,32 @@
-//find difference between something and something
 const app = document.getElementById("app");
 
-const deadline = new Date(2022, 0, 1);
+const form = document.forms[0];
+
+let data = [];
+let deadline = new Date(2022, 0, 1);
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const [timeInput, dateInput] = form.elements;
+  console.log(timeInput.value);
+  const hour = parseInt(timeInput.value[0] + timeInput.value[1]);
+  const minute = parseInt(timeInput.value[3] + timeInput.value[4]);
+
+  console.log(hour);
+  console.log(minute);
+  console.log(new Date(dateInput.value));
+  const date = new Date(dateInput.value);
+  console.log(date.getFullYear());
+  console.log(date.getMonth());
+  console.log(date.getDate());
+  deadline = new Date(
+    date.getFullYear(),
+    date.getMonth(),
+    date.getDate(),
+    hour,
+    minute
+  );
+});
 
 const renderTime = (time, deadline) => {
   const ms = deadline - time;
@@ -14,7 +39,7 @@ const renderTime = (time, deadline) => {
   const d = h / 24;
   const days = Math.round(d);
   return `
- time till next year: ${days} days ${hours} hours ${minutes} minutes ${seconds} seconds
+ time till deadline: ${days} days ${hours} hours ${minutes} minutes ${seconds} seconds
  `;
 };
 
